3e8c9bff080854ae2d3690b067c893e5bd6647c8
mangelcedric <cedric.mangel@gmail.com>
2021-03-03 15:14:21 +0100

    sending email ok just need to display correctly

--- site/src/Controller/MainController.php
+++ site/src/Controller/MainController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class MainController extends AbstractController
 {
@@ -19,12 +26,32 @@ class MainController extends AbstractController
     }
 
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact", name="contact", methods={"GET", "POST"})
      */
-    public function contact(): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
+        $mail = $request->get('contact');
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new TemplatedEmail())
+            ->from($mail['email'])
+            ->to(new Address('cedric.mangel@gmail.com'))
+            ->htmlTemplate('email/email.html.twig')
+            ->text($mail['text'])
+            ->context([
+                'username' => $mail['email'],
+                'text' => $mail['text'],
+                'useremail' => $mail['email']
+                ]);
+            $mailer->send($email);
+            $this->addFlash('success', 'Votre email à bien été envoyé');
+
+            return $this->redirectToRoute('contact');
+        }
+
         return $this->render('main/contact.html.twig', [
-            'controller_name' => 'MainController',
+            'form' => $form->createView(),
         ]);
     }
 }
--- site/src/Form/ContactType.php
+++ site/src/Form/ContactType.php
@@ -3,17 +3,27 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('text', null, [
-                'label' => 'Votre message'
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse email : ',
+                'required' => true,
+                'constraints' => new Assert\NotBlank(),
+            ])
+            ->add('text', TextareaType::class, [
+                'label' => false,
+                'required' => true,
+                'constraints' => new Assert\NotBlank(),
             ])
             ->add('send', SubmitType::class, [
                 'label' => 'Envoyer'
