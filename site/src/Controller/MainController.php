2d104cd52d3a754492b3552dfbb5e73c331bfef7
mangelcedric <cedric.mangel@gmail.com>
2021-03-04 15:35:38 +0100

    download resume ok

--- site/src/Controller/MainController.php
+++ site/src/Controller/MainController.php
@@ -7,8 +7,10 @@ use App\Form\ContactType;
 use App\Repository\LinkRepository;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,6 +24,15 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Route("/cv", name="cv")
+     */
+    public function download()
+    {
+        $file = new File(__DIR__ . '/../../../CV_Cedric_Mangel.pdf');
+        return $this->file($file, 'CV_Cedric_Mangel.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
     }
 
     /**
